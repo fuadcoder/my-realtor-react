6f11ef18e08c154bb543844f50103f1d85154190
Olamide <olasupoolamide00@gmail.com>
2023-04-25 21:07:23 +0100

    Created the header component

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
-/** @type {import('tailwindcss').Config} */
+//** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+} 
 
