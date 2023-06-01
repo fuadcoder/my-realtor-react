aae0c5dc824feece8340443baacd18c67deee7a2
Olamide <olasupoolamide00@gmail.com>
2023-06-01 20:10:40 +0100

    add share copy functionality

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -5,6 +5,7 @@ export default function Spinner() {
     <div className="bg-black bg-opacity-50 flex items-center justify-center fixed left-0 right-0 bottom-0 top-0 z-50">
       <div>
         <img src={spinner} alt="Loading..." className="h-24" />
+        <p className="text-center text-xl font-semibold uppercase py-1 text-white">Loading...</p>
       </div>
     </div>
   )
